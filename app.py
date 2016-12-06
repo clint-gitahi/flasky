8b1535b22c992f3afe832d65df2fa73ea6b93a7e
clint-gitahi <clingitahi@gmail.com>
2016-12-06 17:05:24 +0300

    sec deploy

--- app.py
+++ app.py
@@ -2,17 +2,14 @@
 from flask import Flask, render_template, redirect, \
     url_for, request, session, flash
 from functools import wraps
-from flask_sqlalchemy import SQLAlchemy
-# import sqlite3
+from flask.ext.sqlalchemy import SQLAlchemy
 
 # create the application object
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 # config
 import os
 app.config.from_object(os.environ['APP_SETTINGS'])
-print(os.environ['APP_SETTINGS'])
 
 # create the sqlalchemy object
 db = SQLAlchemy(app)
@@ -20,48 +17,61 @@ db = SQLAlchemy(app)
 # import db schema
 from model import *
 
-# login required decorator 
+
+# login required decorator
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         if 'logged_in' in session:
             return f(*args, **kwargs)
         else:
-            flash('You need to loggin first')
+            flash('You need to login first.')
             return redirect(url_for('login'))
-    return wrap 
+    return wrap
 
-# use decorators to link the funciton to url 
+
+# use decorators to link the function to a url
 @app.route('/')
 @login_required
 def home():
+    # return "Hello, World!"  # return a string
     posts = db.session.query(BlogPost).all()
-    return render_template('index.html', posts=posts)
+    return render_template('index.html', posts=posts)  # render a template
+
 
 @app.route('/welcome')
 def welcome():
-    # render the welcome template.
-    return render_template('welcome.html')
+    return render_template('welcome.html')  # render a template
 
-# handling the login page.
+
+# route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None 
+    error = None
     if request.method == 'POST':
-        if (request.form['username'] != 'admin') or request.form['password'] != 'admin':
-            error = "Invalid login. Please try again."
+        if (request.form['username'] != 'admin') \
+                or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
         else:
             session['logged_in'] = True
-            flash('You are now logged in')
+            flash('You were logged in.')
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
 
 @app.route('/logout')
 @login_required
 def logout():
     session.pop('logged_in', None)
-    flash("You logged out.")
+    flash('You were logged out.')
     return redirect(url_for('welcome'))
 
+
+# connect to database
+# def connect_db():
+#     return sqlite3.connect('posts.db')
+
+
+# start the server with the 'run()' method
 if __name__ == '__main__':
     app.run()
