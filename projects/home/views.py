b482296e1a36b65a344a607b81805207b74ee731
clint-gitahi <clingitahi@gmail.com>
2016-12-07 14:19:34 +0300

    finalizing the messaging system

--- projects/home/views.py
+++ projects/home/views.py
@@ -1,9 +1,10 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, session, flash, Blueprint
+from flask import Flask, render_template, redirect, url_for, session, flash, Blueprint, request
 from functools import wraps
 from projects import app, db  
 from projects.model import BlogPost 
-from flask_login import login_required
+from flask_login import login_required, current_user
+from forms import MessageForm
 
 
 ###############
@@ -24,13 +25,25 @@ home_blueprint = Blueprint(
 
 
 # use decorators to link the function to a url
-@home_blueprint.route('/')
+@home_blueprint.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    # return "Hello, World!"  # return a string
-    posts = db.session.query(BlogPost).all()
-    return render_template('index.html', posts=posts)  # render a template
-
+    error = None
+    form = MessageForm(request.form)
+    if form.validate_on_submit():
+        new_message = BlogPost(
+            form.title.data,
+            form.description.data,
+            current_user.id
+        )
+        print(new_message)
+        db.session.add(new_message)
+        db.session.commit()
+        flash('New entry was successfully posted. Thanks.')
+        return redirect(url_for('home.home'))
+    else:
+        posts = db.session.query(BlogPost).all()
+        return render_template('index.html', posts=posts, form=form, error=error)
 
 
 @home_blueprint.route('/welcome')
